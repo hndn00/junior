c0717dba6904ff233fdae85ae91e78cecc1ab20e
오준서 (o wnstj) <ojunseo1014@naver.com>
2025-05-26 16:40:44 +0900

    Add: Temp File

---
+++ everytime.py
@@ -0,0 +1,30 @@
+from datetime import time
+import requests
+from urllib.parse import urlparse
+
+class Everytime:
+    def __init__(self, path):
+        url = urlparse(path)
+        if url.netloc == "everytime.kr":
+            self.path = url.path.replace("/@", "")
+            return
+        self.path = path
+
+    def get_timetable(self):
+        return requests.post(
+            "https://api.everytime.kr/find/timetable/table/friend",
+            data={
+                "identifier": self.path,
+                "friendInfo": 'true'
+            },
+            headers={
+                "Accept": "*/*",
+                "Connection": "keep-alive",
+                "Pragma": "no-cache",
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Host": "api.everytime.kr",
+                "Origin": "https://everytime.kr",
+                "Referer": "https://everytime.kr/",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+            }).text
